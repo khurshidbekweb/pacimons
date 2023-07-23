41a825f2efada5b3cc4ec3104d0d3f0ac6974a22
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2023-07-23 14:26:13 +0500

    end

--- js/script.js
+++ js/script.js
@@ -2,30 +2,28 @@
 
 let wrapper = document.querySelector(".wrapper");
 pokemons.forEach((e)=>{
-let card = document.createElement("div");
-card.setAttribute("class", "card bg-white rounded-3 p-2")
-card.innerHTML = `
-                        <div class="img text-center position-relative">
-                            <img class="w-75" src="${e.img}" alt="${e.candy}">
-                            <img class="position-absolute" src="./images/heart.svg" alt="like">
-                        </div>
-                        <hr>
-                        <div class="info">
-                            <h2 class="name">${e.name}</h2>
-                            <h5><strong>${e.num}</strong></h5>
-                            <div class="d-flex mb-1 align-items-center justify-content-between">
-                                <h4><strong>Age</strong></h4>
-                                <h4><strong>${e.candy_count}</strong></h4>
-                            </div>
-                            <div class="d-flex mb-2 align-items-center justify-content-between">
-                                <h4>${e.height}</h4>
-                                <h4>${e.weight}</h4>
-                            </div>
-                        </div>
-`
-    wrapper.append(card);
+    let random = Math.floor(Math.random()*50);
+    let card = creatElemen("div", "card bg-white rounded-3 p-2", `
+    <div class="img text-center position-relative">
+        <img class="w-75" src="${e.img}" alt="${e.candy}">
+        <img class="position-absolute" src="./images/heart.svg" alt="like">
+    </div>
+    <hr>
+    <div class="info">
+        <h2 class="name">${e.name}</h2>
+        <h5><strong>${e.num}</strong></h5>
+        <div class="d-flex mb-1 align-items-center justify-content-between">
+            <h4><strong>Age</strong></h4>
+            <h4><strong>${e.candy_count ? e.candy_count : random}</strong></h4>
+        </div>
+        <div class="d-flex mb-2 align-items-center justify-content-between">
+            <h4>${e.height}</h4>
+            <h4>${e.weight}</h4>
+        </div>
+    </div>
+`)
+wrapper.append(card);
 })
 
-
-
-
+let card = creatElemen("div")
+console.log(card);
--- js/utlis.js
+++ js/utlis.js
@@ -2,3 +2,16 @@
 function $(item){
     return document.querySelector(item);
 }
+
+function creatElemen(tagName,className,content){
+    let newElement = document.createElement(tagName);
+    if(className){
+        newElement.setAttribute("class", className);
+    }
+    if(content){
+        newElement.innerHTML = content;
+    }
+    
+
+    return newElement;
+}
